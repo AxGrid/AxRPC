8ba349b647c5d619af490eb3c25a2c1e399e4aca
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2020-03-07 17:57:03 +0100

    Add filters

--- AxRPCWeb/src/main/java/com/axgrid/rpc/web/handler/AxRPCDescriptionHandler.java
+++ AxRPCWeb/src/main/java/com/axgrid/rpc/web/handler/AxRPCDescriptionHandler.java
@@ -6,9 +6,12 @@ import com.axgrid.rpc.web.service.AxRPCWebService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/ax-rpc/v1/")
@@ -18,8 +21,15 @@ public class AxRPCDescriptionHandler {
     AxRPCWebService service;
 
     @GetMapping("/description.json")
-    List<AxRPCDescription> services() {
-        return service.getServices();
+    List<AxRPCDescription> services(@RequestParam(required = false, defaultValue = "") String requestObject, @RequestParam(required = false, defaultValue = "") String responseObject) {
+        if (requestObject.equals("") && responseObject.equals(""))
+            return service.getServices();
+        Stream<AxRPCDescription> stream = service.getServices().stream();
+        if (!requestObject.equals(""))
+            stream = stream.filter(item -> item.getRequestObjectFullName().equals(requestObject));
+        if (!responseObject.equals(""))
+            stream = stream.filter(item -> item.getResponseObjectFullName().equals(responseObject));
+        return stream.collect(Collectors.toList());
     }
 
 }
